17a45dfae55f6881e7db8b00a1b3943287383ba4
ebriceno13 <enrique.briceno.martinez@ucreativa.com>
2021-07-28 21:03:34 -0600

    Selenium Framework Commit#1

---
+++ SeleniumFramework/src/main/java/TestAccount.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestAccount {
+    String userName = "juan.piedra@ucreativa.com";
+    String password = "asdf";
+
+    @Test
+    public void Test_Login_Successfull(){
+        String pathToDriver = Test.class.getResource("/chromedriver.exe").getPath();
+        System.setProperty("webdriver.chrome.driver", pathToDriver);
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demo.opencart.com");
+
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        /*
+        driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        */
+
+        //Go to login page
+        driver.findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[2]/a/span[1]")).click();
+        driver.findElement(By.linkText("Login")).click();
+
+        //Llenar formulario
+        driver.findElement(By.name("email")).sendKeys("");
+        driver.findElement(By.name("password")).sendKeys("");
+        driver.findElement(By.name("[value='Login']")).click();
+
+        WebElement logOutButton = driver.findElement(By.linkText("Logout"));
+        Assert.assertTrue(logOutButton.isDisplayed());
+
+    }
+
+}
